42e1c89de3d68fae3aab721abc67d771b76ae1e4
rurui <rurui739759848@live.com>
2018-07-14 22:40:55 +0800

    loop

--- test.py
+++ test.py
@@ -39,3 +39,38 @@ elif bmi<32:
     print("obesity")
 else:
     print("very obesity")
+#loop
+names=['wzh','whvihs','rurui']
+for name in names:
+    print(name)
+#the sum of 1 to 100
+sum=0
+for x in range(101):
+    sum=sum+x
+print(sum)
+#while
+sum=0
+n=99
+while n>0:
+    sum=sum+n
+    n=n-2
+print(sum)
+#loop values
+values=["111","222","3333"]
+for a in values:
+    print("hello "+a)
+#break
+n=0
+while n<=100:
+    if n>10:
+        break
+    print(n)
+    n=n+1
+print("end")
+#continue
+num=0
+while num<10:
+    num=num+1
+    if num%2==0:
+        continue
+    print(num)
