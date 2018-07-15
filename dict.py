ce87565adca6ddb0b21dea704b099703af871771
rurui <rurui739759848@live.com>
2018-07-15 15:49:12 +0800

    function

--- dict.py
+++ dict.py
@@ -4,3 +4,17 @@ d["wzh"]
 list=(1,2,3)
 d[list]="1111"
 d
+#hex()
+n=100
+print(hex(n))
+#my_abs
+def my_abs(x):
+    if not isinstance(x,(int,float)):
+        raise TypeError("bad operand type")
+    if x>0:
+        return x
+    else:
+        return -x
+#print(my_abs(-99))  
+my_abs("1")
+abs("1")
