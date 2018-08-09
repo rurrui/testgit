0b2c12030da13effc18fa1280013b9d9b3cb3e39
rurui <rurui739759848@live.com>
2018-08-09 22:56:38 +0800

    customizeclass

---
+++ customizeclass.py
@@ -0,0 +1,24 @@
+class Student(object):
+    def __init__(self,name):
+        self.name=name
+    def __str__(self):      # __str__可以把Student的实例格式化为正常字符串形式（以print输出时有效）
+         return 'Student object (name: %s)' % self.name
+    __repr__=__str__        #在py交互命令模式下，使以字符串形式输出
+stu1=Student('wzh')
+print(stu1)
+# iter()
+class Fob(object):
+    def __init__(self):
+        self.a,self.b=0,1
+    def __iter__(self):
+        return self
+    def __next__(self):
+        self.a,self.b=self.b,self.a+self.b
+        if self.a>1000:
+            raise StopIteration()
+        return self.a
+for n in Fob():
+    print(n)
+class Person(object):
+    pass
+dir[Person]
