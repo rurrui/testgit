de3e06fce583e8c047325e47e30a97bd1e5b6692
rurui <rurui739759848@live.com>
2018-08-18 01:24:42 +0800

    io

--- io.py
+++ io.py
@@ -3,9 +3,17 @@ import io
 try:
     f=open(r'C:\Users\rurui\Desktop\hello.txt','r')
     print(f.read())
+
 finally:
     if f:
         f.close()
 # 可以使用一种更为简单的语法糖，引入with语句帮我们执行f.close()
+print("================================================")
 with open(r'C:\Users\rurui\Desktop\hello.txt','r') as f:
-    print(f.read())
+    print(f.read(-1))
+    print("=================================================")
+    line=f.readline()
+    #print(f.readline(-1)
+f1=open('xyz.txt','r')
+for line in open('xyz.txt'):
+    print(line)
