47851c3038fa505a37e12002c47ec0b477cdaad3
rurui <rurui739759848@live.com>
2018-08-17 23:16:59 +0800

    with

--- io.py
+++ io.py
@@ -1,4 +1,11 @@
 import io
-f=open(r'C:\Users\rurui\Desktop\hello.txt','r')
-print(f.read())
-f.close()
+
+try:
+    f=open(r'C:\Users\rurui\Desktop\hello.txt','r')
+    print(f.read())
+finally:
+    if f:
+        f.close()
+# 可以使用一种更为简单的语法糖，引入with语句帮我们执行f.close()
+with open(r'C:\Users\rurui\Desktop\hello.txt','r') as f:
+    print(f.read())
