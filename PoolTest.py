5a4cd3fe1ced3a0683d51797a9502b8d2e2b88a6
rurui <rurui739759848@live.com>
2018-08-30 22:56:30 +0800

    30/8/2018

---
+++ PoolTest.py
@@ -0,0 +1,12 @@
+from multiprocessing import Pool
+import  os
+
+
+def f(x):
+    print('SubProcess id is %s' % os.getpid())
+    return x * x
+
+
+if __name__ == '__main__':
+    with Pool(5) as p:
+        print(p.map(f, [1, 2, 3]))
