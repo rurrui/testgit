8a7ce3aaf62e255bc9355d892fd85a1986ab7a5a
rurui <rurui739759848@live.com>
2018-09-03 23:43:58 +0800

    not thread local

--- Thread/Thread_Local.py
+++ Thread/Thread_Local.py
@@ -3,6 +3,8 @@ import threading
 # 为了让每个线程使用不同的变量，我们可以使用threadlocal来实现
 # 创建一个全局thread_local变量
 global_school = threading.local()
+global_name = threading.local()
+student_name = 'default'
 
 
 def process_student():
@@ -16,9 +18,26 @@ def process_thread(name):
     process_student()
 
 
+def process_share(name):
+    global student_name
+    student_name = name
+    print('Hello,%s (in %s)' % (name, threading.current_thread().name))
+
+
 t1 = threading.Thread(target=process_thread, args=('wzh',), name='thread_1')
 t2 = threading.Thread(target=process_thread, args=('rurui',), name='thread_2')
 t1.start()
 t2.start()
+
 t1.join()
 t2.join()
+t3 = threading.Thread(target=process_share, args=('lyw',), name='thread_3')
+t4 = threading.Thread(target=process_share, args=('wq',), name='thread_4')
+t3.start()
+print('==================')
+print(student_name)
+t4.start()
+print('==================')
+print(student_name)
+t3.join()
+t4.join()
